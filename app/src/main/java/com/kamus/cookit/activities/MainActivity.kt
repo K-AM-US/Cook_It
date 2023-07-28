dfbed0c332edcbf6150b328f94f477a86448c740
mauriciocasillas <kamusgallagher@gmail.com>
2023-07-28 01:55:32 -0600

    SplashScreen. Bottom Navigation View.

--- app/src/main/java/com/kamus/cookit/activities/MainActivity.kt
+++ app/src/main/java/com/kamus/cookit/activities/MainActivity.kt
@@ -2,11 +2,43 @@ package com.kamus.cookit.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.kamus.cookit.R
+import com.kamus.cookit.databinding.ActivityMainBinding
+import com.kamus.cookit.fragments.AccountFragment
+import com.kamus.cookit.fragments.HomeFragment
+import com.kamus.cookit.fragments.SearchFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomMenu.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.btnHome -> {
+                    replaceFragment(HomeFragment.newInstance())
+                    true
+                }
+                R.id.btnSearch -> {
+                    replaceFragment(SearchFragment.newInstance())
+                    true
+                }
+                R.id.btnAccount ->{
+                    replaceFragment(AccountFragment.newInstance())
+                    true
+                }
+                else -> false
+            }
+        }
+        replaceFragment(HomeFragment.newInstance())
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit()
     }
 }
--- app/src/main/java/com/kamus/cookit/activities/SplashScreen.kt
+++ app/src/main/java/com/kamus/cookit/activities/SplashScreen.kt
@@ -1,5 +1,6 @@
 package com.kamus.cookit.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -8,7 +9,13 @@ import com.kamus.cookit.R
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+        val splashScreen = installSplashScreen()
         setContentView(R.layout.activity_splash_screen)
+
+        splashScreen.setKeepOnScreenCondition{ true }
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
